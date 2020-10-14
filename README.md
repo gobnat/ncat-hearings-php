430003e209bbee8f8fc626ce4a31443c80c08035
gobnat <rafael.mazzoldi@gmail.com>
2020-10-15 00:02:37 +1100

    ready for testing

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+## Example of DOM on a single Venue list page.
+
+```
+<span id="lblSubHeader14102020" class="clsGridItem" style="font-weight:bold;">3:30 PM Wednesday, 14 Oct 2020 at Room 15.6 Level 15, 66 Goulburn Street Sydney NSW</span>
+<hr />
+<table cellspacing="0" id="dg14102020" style="width:100%;border-collapse:collapse;">
+	<tr class="clsGridItem">
+		<td style="width:15%;">RT 20/36940</td><td style="width:30%;">SHENG ZHENG</td><td style="width:55%;">VANESSA SOFIAN</td>
+	</tr>
+</table>
+```
--- scraper.php
+++ scraper.php
@@ -1,20 +1,112 @@
-<?
+<?php
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'vendor/autoload.php';
+
+//require 'scraperwiki.php';
+require 'vendor/openaustralia/scraperwiki/scraperwiki.php';
+require 'vendor/openaustralia/scraperwiki/simple_html_dom.php';
+//require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("http://www.ncat.nsw.gov.au/Pages/going_to_the_tribunal/hearing_lists.aspx");
 //
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+// Load the dom
+$dom = new simple_html_dom();
+$dom->load($html);
+
+// Look through all tables and get an hyperlinks to venues.
+// Currently only one table appears. Long term this will not be the case. Need a way to to properly grab only Consumer matters
+foreach($dom->find('table tr td a') as $e){
+  //store the Location, Link, Postcode
+   $links[] = array(
+     "location" => $e->innertext,
+     "url" => $e->href,
+     "postcode" => substr($e->href,-4)
+   );
+}
+
+//Below is silly because PHP and dates is silly.
+// Set timezone
+date_default_timezone_set('Australia/Sydney');
+
+//30 days cycle. eed start and end date
+$startDate = new \DateTime(date('Y-m-d', time()));
+$endDate = new \DateTime(date('Y-m-d', strtotime("+30 days")));
+//Intervak for the loop
+$interval = \DateInterval::createFromDateString('1 day');
+$period = new \DatePeriod($startDate, $interval, $endDate);
+
+
+//Loop through venues
+foreach ($links as $link) {
+  //echo $link["location"] . " | " . $link["url"] . " | " . $link["postcode"] . "<br>";
+  $url = $link["url"];
+  //echo $url . "<br>";
+  //$url = 'https://online.ncat.nsw.gov.au/Hearing/HearingList.aspx?LocationCode=2000';
+  $dom->load(scraperwiki::scrape($url));
+
+  //Loop through each date and collect results
+  foreach ($period as $date) {
+    //Tell people what you are doing
+    echo "Processing " . $date->format("d-m-Y") . " for " . $link["postcode"] . "<br>\n";
+    //make produce the id used to find eac hearing table
+    $id = "dg" . $date->format("dmY");
+    //echo $id . "<br>";
+    foreach($dom->find('table[id=' . $id . '] tr.clsGridItem') as $e){
+      if (is_null($e)) {
+        //nothing
+        echo "no item <br>";
+      }else{
+        //Grab the time and place infor from span above table
+        /**
+        *
+        *
+        **/
+
+
+        if (is_null($e->parent()->prev_sibling()->prev_sibling()->find('span'))) {
+          $time_and_place = $e->parent()->prev_sibling()->innertext;
+
+        }else {
+          $time_and_place = $e->parent()->prev_sibling()->prev_sibling()->innertext;
+        }
+      }
+      //Need to spit out time first with preg_match cause... php... probably a better way to do this but I cant find it.
+      preg_match('/(^.*(A|P)M)/', $time_and_place, $time_matches);
+
+      //Create aour NCAT case object/array
+      $ncat_case = array(
+        'unique_id'       => $date->format("d-m-Y") . $e->find('td')[0]->innertext,
+        'case_number'     => $e->find('td')[0]->innertext,
+        'party_a'         => $e->find('td')[1]->innertext,
+        'party_b'         => $e->find('td')[2]->innertext,
+        'date'            => $date->format("d-m-Y"),
+        'time'            => $time_matches[0],
+        'location'        => preg_split('/ at /', $time_and_place)[1],
+        'venue'           => $link["location"],
+        'venue_postcode'  => $link["postcode"]
+      );
+
+      // // Write out to the sqlite database using scraperwiki library
+      //scraperwiki::save_sqlite(array($ncat_case['unique_id']), $ncat_case);
+      $res[] = $ncat_case;
+    } //each id
+  } //end period
+}//end link
+
+
+//print_r($res);
+
+
+
+
+
+
 //
 // // An arbitrary query against the database
 // scraperwiki::select("* from data where 'name'='peter'")
@@ -24,4 +116,5 @@
 // All that matters is that your final data is written to an SQLite database
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
+
 ?>
